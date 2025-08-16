be035383499924f71ab316d595437e6cd4817ba3
Usman Iqbal <usmaniqbal912@outlook.com>
2025-08-16 10:02:19 +0100

    updated hero section animation and added improved styling to the footer

--- docs/js/home.js
+++ docs/js/home.js
@@ -1,9 +1,25 @@
 var typed = new Typed("#typed-text", {
-  strings: ["Usman Iqbal", "MSc Computer Science", "Web Developer", "Software Developer"],
+  strings: [
+    "Usman Iqbal",
+    "MSc Computer Science",
+    "Web Developer",
+    "Software Developer",
+  ],
   typeSpeed: 50,
   backSpeed: 40,
   backDelay: 1800,
   loop: true,
   showCursor: true,
   cursorChar: "|",
+});
+
+tsParticles.load("tsparticles", {
+  fpsLimit: 30,
+  particles: {
+    number: { value: 40 },
+    links: { enable: true, distance: 120, opacity: 0.3 },
+    move: { enable: true, speed: 0.5 },
+    size: { value: 2 },
+  },
+  interactivity: { events: {} },
 });
